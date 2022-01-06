296c206ba82615c7b499df89d94efb44d6f65efa
tanminkwan <tanminkwan@gmail.com>
2022-01-06 12:18:42 +0900

    Update config.py
    SWAGGER is activated

--- config.py
+++ config.py
@@ -106,6 +106,8 @@ IMG_UPLOAD_URL = "/static/uploads/"
 # Setup image size default is (300, 200, True)
 # IMG_SIZE = (300, 200, True)
 
+FAB_API_SWAGGER_UI = True
+
 # Theme configuration
 # these are located on static/appbuilder/css/themes
 # you can create your own and easily use them placing them on the same dir structure to override
