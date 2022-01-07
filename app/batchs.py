0c7da127635d74dc3f4e40bdc5f24e34476f953a
tanminkwan <tanminkwan@gmail.com>
2022-01-07 18:29:12 +0900

    common queries added
    common queries added

--- app/batchs.py
+++ app/batchs.py
@@ -1,7 +1,15 @@
 from . import db
+from .queries import selectRows
+
 from datetime import datetime, timedelta
 
 def batch_transVideos():
     
     print('batch_transVideos : ', datetime.now())
+    filter_dict = dict(name='테스트')
+    recs = selectRows('test_table',filter_dict)
+    
+    for rec in recs:
+        print('row : ', rec)
+    
     db.session.commit()
