64d54575503a9ad764bd005c42f90d1b2d3df0e3
tanminkwan <tanminkwan@gmail.com>
2022-01-10 14:24:57 +0900

    manifest 정보 update 기능 추가
    manifest 정보 update 기능 추가

--- app/scheduled_jobs.py
+++ app/scheduled_jobs.py
@@ -1,6 +1,21 @@
 from . import db, scheduler
-from .batchs import batch_transVideos
+from .batchs import updateContentsInfo, batch_transVideos
+from datetime import datetime, timedelta
 
 @scheduler.task('cron', id='job_transVideos', name='Auto Transcoding Videos', minute='*/30')
 def job_transVideos():
     batch_transVideos()
+    
+def job_create_job(target):
+    
+    dynamic_dict = dict(trigger = 'date'\
+        , run_date = datetime.now() + timedelta(seconds=10))
+    
+    scheduler.add_job(
+        id = 'run_get_m8u3_4_' + target.stored_filename
+      , name = 'video transecoding'
+      , func = updateContentsInfo
+      , args = (target.stored_filename,)
+      , **dynamic_dict
+    )
+    
